dd3698df5917b2fe38d46176f3dff7beddf07fe8
Maria Saldina <sald1na.maria@yandex.ru>
2024-04-22 00:58:39 +0400

    Fixed graph homework (now vertices can be int or char) (CSP)

--- CSP/graph/graph.cpp
+++ CSP/graph/graph.cpp
@@ -3,16 +3,19 @@
 #include <Windows.h>
 #include <algorithm>
 #include <string>
+#include <map>
 
 using namespace std;
 
+template <typename T>
 struct queue{ // очередь
-    int inf;
+    T inf;
     queue *next;
 };
 
-void push_queue(queue *&h, queue *&t, int x){ // добавление элемента в конец очереди
-    queue *r = new queue;
+template <typename T>
+void push_queue(queue<T> *&h, queue<T> *&t, T x){ // добавление элемента в конец очереди
+    queue<T> *r = new queue<T>;
     r->inf = x;
     r->next = nullptr;
     if (!h && !t){
@@ -24,61 +27,65 @@ void push_queue(queue *&h, queue *&t, int x){ // добавление элемента в конец оче
     }
 }
 
-int pop_queue(queue *&h, queue *&t){ // удаление элемента из начала очереди
+template <typename T>
+T pop_queue(queue<T> *&h, queue<T> *&t){ // удаление элемента из начала очереди
     int x = h->inf;
-    queue *p = h;
+    queue<T> *p = h;
     h = h->next;
     if (!h) t = nullptr;
     delete p;
     return x;
 }
 
+template <typename T>
 struct stack{ // стек
-    int inf;
+    T inf;
     stack *next;
 };
 
-void push_stack(stack *&h, int x){ // добавление элемента в стек
-    stack *r = new stack;
+template <typename T>
+void push_stack(stack<T> *&h, T x){ // добавление элемента в стек
+    stack<T> *r = new stack<T>;
     r->inf = x;
     r->next = h;
     h = r;
 }
 
-int pop_stack(stack *&h){ // удаление элемента из стека
-    stack *r = h;
+template <typename T>
+T pop_stack(stack<T> *&h){ // удаление элемента из стека
+    stack<T> *r = h;
     int x = h->inf;
     h = h->next;
     delete r;
     return x;
 }
 
-void bfs(vector<vector<int>> gr, vector<bool> &used, int n, int x){ // обход графа в ширину
+template <typename T>
+void bfs(map<T, vector<T>> gr, map<T, bool> &used, int n, T x){ // обход графа в ширину
     used[x] = 1; // помечаем первую вершину как посещённую
     cout << x << " ";
-    queue *h = nullptr, *t = nullptr; // создаём очередь
+    queue<T> *h = nullptr, *t = nullptr; // создаём очередь
     push_queue(h, t, x); // помещаем вершину в очередь
     while (h){ // пока очередь не пуста
         x = pop_queue(h, t); // удаляем голову из очереди
         for (int i = 0; i < gr[x].size(); ++i){ // проходимся по списку смежных с текущей вершин
             if (!used[gr[x][i]]){ // если эту вершину ещё не посетили
-                int y = gr[x][i];
+                T y = gr[x][i];
                 used[y] = 1; // помечаем её как посещённую
                 push_queue(h, t, y); // и добавляем в очередь
                 cout << y << " ";
             }
         }
     }
-    auto not_used = find(used.begin() + 1, used.end(), 0); // если остались непосещённые вершины
-    if (not_used != used.end()) bfs(gr, used, n, *not_used);
 }
 
-void dfs(vector<vector<int>> gr, vector<bool> &used, int n, int x){ // обход графа в глубину
-    stack *h = nullptr; // создаём стек
+template <typename T>
+void dfs(map<T, vector<T>> gr, map<T, bool> &used, int n, T x){ // обход графа в глубину
+    stack<T> *h = nullptr; // создаём стек
     push_stack(h, x); // добавляем в него первую вершину
     used[x] = 1; // помечаем её как посещённую
     cout << x << " ";
-    bool flag = 0; int y;
+    bool flag = 0; T y;
     while (h){ // пока стек не пуст
         x = h->inf; // рассматриваем голову стека
         for (int i = 0; i < gr[x].size(); ++i){ 
@@ -96,46 +103,51 @@ void dfs(vector<vector<int>> gr, vector<bool> &used, int n, int x){ // обход гра
         else pop_stack(h); // иначе удаляем голову стека
         flag = 0;
     }
-    auto not_used = find(used.begin() + 1, used.end(), 0); // если остались непосещённые вершины
-    if (not_used != used.end()) dfs(gr, used, n, *not_used);
 }
 
-vector<vector<int>> create_graph(int &n){ // считывем граф
-    cout << "Enter the number of vertices: ";
-    int x, y; cin >> n; n++;
-    string s;
-    vector<vector<int>> graph(n);
-    cout << "Enter the graph according to this format: <vertice> : <v_1> <v_2> ... <v_n> ;\n";
-    for (int i = 1; i < n; ++i){
-        vector<int> a;
-        cin >> x >> s;
-        while (s != ";"){
-            cin >> s;
-            if (isdigit(s[0])){
-                y = stoi(s);
-                a.push_back(y);
-            }
+template <typename T>
+map<T, vector<T>> create_graph(int &n, T){ // считываем граф
+    cout << "Введите количество вершин: ";
+    cin >> n;
+    T x, y;
+    int m; 
+    map<T, vector<T>> graph;
+    cout << "Введите вершины в соответствии с форматом: <вершина> <число_связанных_вершин> <вершина_1> <вершина_2> ... <вершина_n>\n";
+    for (int i = 0; i < n; ++i){
+        cin >> x >> m;
+        vector<T> a(m);
+        for (int j = 0; j < m; ++j){
+            cin >> y;
+            a[j] = y;
         }
         graph[x] = a;
     }
     return graph;
 }
 
-void task(){
-    int n, x;
-    vector<vector<int>> gr = create_graph(n);
-    vector<bool> used(n, 0);
-    cout << "Enter the vertice to start with: ";
+template <typename T>
+void task(T c){
+    int n; T x;
+    map<T, vector<T>> gr = create_graph(n, c);
+    map<T, bool> used;
+    for (auto it = gr.begin(); it != gr.end(); ++it) used[it->first] = 0;
+    cout << "Введите вершину, с которой начинается обход в ширину: ";
     cin >> x;
     bfs(gr, used, n, x);
-    for (int i = 0; i < n; ++i) used[i] = 0;
+    for (auto it = used.begin(); it != used.end(); ++it) it->second = 0;
     cout << endl;
-    cout << "Enter the vertice to start with: ";
+    cout << "Введите вершину, с которой начинается обход в глубину: ";
     cin >> x;
     dfs(gr, used, n, x);
 }
 
 int main(){
-    task();
+    SetConsoleCP(1251); // установка кодировки win-cp 1251 в поток ввода
+    SetConsoleOutputCP(1251); // установка кодировки win-cp 1251 в поток вывода
+    string type;
+    cout << "Выберите тип вершин графа (введите integer/char): ";
+    cin >> type;
+    if (type == "integer") task(0);
+    else task('a');
     return 0;
 }
